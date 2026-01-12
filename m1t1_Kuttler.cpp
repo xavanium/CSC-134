7ba82fe66df652c8759c871711164e3b36fd3d3a
Xavanium <xavanium@gmail.com>
2026-01-12 18:58:01 +0000

    started m1t1

---
+++ m1t1_Kuttler.cpp
@@ -0,0 +1,14 @@
+//CSC 134
+// M1T1 (Module 1, Tutorial 1)
+
+
+//We'll explain this later
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Say hello
+    cout << "Hello, World!" << endl;
+    cout << endl; //blank line
+    return 0; //exit with no errors
+}
