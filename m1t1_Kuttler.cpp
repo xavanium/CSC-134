c891ea3d12ad17f6d0447e27da0472853eea1c69
Xavanium <xavanium@gmail.com>
2026-01-12 19:41:11 +0000

    input and math

--- m1t1_Kuttler.cpp
+++ m1t1_Kuttler.cpp
@@ -11,5 +11,20 @@ int main() {
     // Say hello
     cout << "Hello, World!" << endl;
     cout << endl; //blank line
+    
+    string name;
+    cout << "Input your name: ";
+    cin >> name;
+    cout << "Hello, " << name << ", welcome to C++!" << endl;
+    cout << endl;
+    int num;
+    cout << "Input a number: ";
+    cin >> num;
+    int exponent;
+    cout << "Input the exponent for this number: ";
+    cin >> exponent;
+    int result = pow(num, exponent);
+    cout << num << " raised to the power of " << exponent << " is " << result << endl;
+    cout << endl;
     return 0; //exit with no errors
 }
