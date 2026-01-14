45204a9109fc889d2575551e572c285cb96ee21a
Xavanium <xavanium@gmail.com>
2026-01-14 18:59:42 +0000

    idk

--- m1lab_Kuttler.cpp
+++ m1lab_Kuttler.cpp
@@ -16,8 +16,19 @@ int main() {
     //Declare variables at the top, it's easier to read
     string item = "bananas"; //change to any thing you want to sell
     int item_count = 100;
+    int purchased; //how many user wants
     double price_each = 0.79;
+    double total_price;
     cout << "Welcome to our " << item << " store." << endl;
+    cout << "We have " << item_count << " " << item << "." << endl;
+    cout << "They cost $" << price_each << " each" << endl;
+    //Find out how much user wants to buy
+    // Calculate the total
+    cout << "How much would you like to buy? ";
+    cin >> purchased;
+    total_price = purchased * price_each;
+    cout << "You bought " << purchased << " " << item << "." << endl;
 
+    cout << "The total price is $" << total_price << endl;
     return 0;
 }
