03e91fa6b7e05d15d400cc00dd1131366086e27d
Xavanium <xavanium@gmail.com>
2026-01-21 19:44:11 +0000

    movie time?

---
+++ m1/m1hw1_Kuttler.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    string movie_name = "Austin Powers: The Spy Who Shagged Me";
+    int year_made = 1999;
+    double money_made = 33;
+    cout  << "The movie " << movie_name << " came out in " << year_made << " and made $" << money_made << " million." << endl;
+    cout << "\"Just make love to me. Now, Austin Powervich! Hurry!\"" << endl;
+    cout << "\"Oh, you are hairy like animal! Grr, baby! Very grr!\"" << endl;
+    cout << "\"I'm not even shooting you. It's crazy.\"" << endl;
+    return 0;
+}
