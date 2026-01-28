6f6420d28940eb58e45f35e45b020155a56533a5
Xavanium <xavanium@gmail.com>
2026-01-28 18:38:22 +0000

    started m2lab1

---
+++ m2/m2lab1_Kuttler.cpp
@@ -0,0 +1,35 @@
+/*
+CSC 134
+M2LAB1 - Crates
+Kuttlerj6796
+1/28/26
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    //Part 1 Set up variables
+    //crate variables
+    double length, width, height; //in feet
+    //Price variabkes
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+    double cost, customer_price, profit; //in $
+    //Part 2 input
+    cout << "Crate Purchase Program" << endl;
+    cout << "Enter the size of your crate \n";
+    cout << "Length? ";
+    cin >> length;
+    cout << "Width? ";
+    cin >> width;
+    cout << "Height? ";
+    cin >> height;
+    //Part 3 calculation
+    
+    //part 4 output
+
+    return 0;
+}
