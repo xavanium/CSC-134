0ea1b149d6ca85d108b2446c6f98bd22b3fb0037
Xavanium <xavanium@gmail.com>
2026-02-11 18:54:31 +0000

    M2HW1 started

---
+++ m2/m2hw1_Kuttler.cpp
@@ -0,0 +1,12 @@
+/* */
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Question 1: " << endl;
+    //question 1 code here
+    cout << "Question 2: " << endl;
+    //question 2 code here
+    cout << "Question 3: " << endl;
+    //question 3 code here
+    return 0;
+}
