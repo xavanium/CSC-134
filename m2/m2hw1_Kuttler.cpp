fe0137dfc42497b65c5b18d9ce1e21635c8fee82
Xavanium <xavanium@gmail.com>
2026-02-11 19:16:28 +0000

    completed q2

--- m2/m2hw1_Kuttler.cpp
+++ m2/m2hw1_Kuttler.cpp
@@ -35,9 +35,41 @@ int main(){
     cout << "Final balance: $" << endBal << endl;
 
 
-
-    cout << "Question 2: " << endl;
+    cout << "Question 2: \n" << endl;
     //question 2 code here
+     //Part 1 Set up variables
+    //crate variables
+    double length, width, height, volume; //in feet
+    //Price variabkes
+    const double COST_PER_CUBIC_FOOT = 0.3;
+    const double CHARGE_PER_CUBIC_FOOT = 0.52;
+    double cost, customer_price, profit; //in $
+    //Part 2 input
+    cout << "Crate Purchase Program" << endl;
+    cout << "Enter the size of your crate \n";
+    cout << "Length? ";
+    cin >> length;
+    cout << "Width? ";
+    cin >> width;
+    cout << "Height? ";
+    cin >> height;
+    //Part 3 calculation
+    volume = length * width * height;
+    cost = volume * COST_PER_CUBIC_FOOT;
+    customer_price = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = customer_price - cost;
+
+    //part 4 output
+    cout << setprecision(2) << fixed; // 2 decimal places
+    cout << "\n---- CRATE INFO ----\n";
+    cout << "Volume: \t\t" << volume << " cubic feet.\n";
+    cout << "Cost of production: \t$" << cost << endl;
+    cout << "Retail price: \t\t$" << customer_price << endl;
+    cout << "Profit: \t\t$" << profit; 
+    cout << "\n---- THE STATS ----\n";
+    cout << "THANK YOU COME AGAIN\n" << endl;
+    // finished i guess ?
+
     cout << "Question 3: " << endl;
     //question 3 code here
     return 0;
