7f78226b9a15033ef123c4a475043d213372ff9a
Xavanium <xavanium@gmail.com>
2026-02-18 19:01:04 +0000

    broked it and fixed it

--- M3/M3T3_Kuttler.cpp
+++ M3/M3T3_Kuttler.cpp
@@ -15,7 +15,7 @@ int roll(); //returns number from 1-6
 
 int main(){
     //seed the RNG
-    int roll1, roll2, total;
+    int roll1, roll2, total, point;
     string status; // win, lose
     srand(time(0));
     roll1 = roll();
@@ -24,6 +24,18 @@ int main(){
 
     //print roll
     cout << "Roll is: 🎲 " << roll1 << " + " << roll2 << " = " << total << endl;
+    // determine win or loss
+    if (total == 7 || total == 11){ // || is OR
+        status = "win";
+    } else if (total == 2 || total == 3 || total == 12) {
+        status = "loss";
+    }
+    else {
+        //point
+        point = total; //save for later
+        status = "point";
+    }
+    cout << "Roll results: " << status << endl;
     return 0; 
 }
 
