dbee6d1b13a386d5eb48301861c85519a507bf1c
Xavanium <xavanium@gmail.com>
2026-02-18 18:25:49 +0000

    started m3t3

---
+++ M3/M3T3_Kuttler.cpp
@@ -0,0 +1,14 @@
+/* M3T3 - Craps & Random Numbers
+Kuttlerj6796
+CSC 134
+2/18/26
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main(){
+
+    return 0;
+}
