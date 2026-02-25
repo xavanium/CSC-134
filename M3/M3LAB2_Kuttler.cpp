ae6904ab4b1abfc93f37db45d364e489b8ec82ae
Xavanium <xavanium@gmail.com>
2026-02-25 18:14:58 +0000

    wrote the code for m3lab2

---
+++ M3/M3LAB2_Kuttler.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int numGrade;
+    char grade;
+    cout << "Enter your numerical grade: ";
+    cin >> numGrade;
+    if (numGrade>=90 && numGrade<=100){
+       grade = 'A'; 
+    } else if (numGrade>=80 && numGrade<=89){
+        grade = 'B';
+    } else if (numGrade>=70 && numGrade<=79){
+        grade = 'C';
+    } else if (numGrade>=60 && numGrade<=69){
+        grade = 'D';
+    } else {
+        grade = 'F';
+    }
+    cout << "Your grade is " << grade << endl << endl;
+    return 0;
+}
