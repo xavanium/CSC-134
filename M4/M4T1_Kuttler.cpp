69736d3c06df6bd8667977c9389f728549f43be4
Xavanium <xavanium@gmail.com>
2026-03-04 18:22:55 +0000

    finished m4t1

--- M4/M4T1_Kuttler.cpp
+++ M4/M4T1_Kuttler.cpp
@@ -10,6 +10,18 @@ Kuttlerj6796
 using namespace std;
 
 int main(){
-    
+    //DEMO - List of numbers and squares
+    const int MIN = 1;
+    const int MAX = 10;
+    int num = MIN;
+    int squared; // holds the square
+    cout << "Number\tNum Squared" << endl;
+    cout << "___________________" << endl;
+    while (num <= MAX){
+        squared = num*num;
+        cout << num << "\t" << squared << endl;
+        num++; // next number
+    }
+
     return 0;
 }
