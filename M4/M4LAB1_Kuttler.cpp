0e390df36f3dff28e865e1dae581ea4da4709db2
Xavanium <xavanium@gmail.com>
2026-03-09 17:32:05 +0000

    started m4lab1

---
+++ M4/M4LAB1_Kuttler.cpp
@@ -0,0 +1,14 @@
+/*m4lab1 - nested loops
+kuttlerj6796
+3/9/26
+*/
+
+#include <iostream>
+using namespace std;    
+int main(){
+    char symbol = 'X';
+    string emoji = "🥹";
+    cout << symbol << endl;
+    cout << emoji << endl;
+    return 0;
+}
