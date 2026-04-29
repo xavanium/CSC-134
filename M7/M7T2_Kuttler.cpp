3cfb3e140a2a0960957503ad4e82c5b63fc53fe8
Xavanium <xavanium@gmail.com>
2026-04-29 17:43:52 +0000

    continued

--- M7/M7T2_Kuttler.cpp
+++ M7/M7T2_Kuttler.cpp
@@ -4,6 +4,9 @@ Kuttlerj6796
 4/29/26 */
 
 #include <iostream>
+//function prototypes
+std::string setName();
+void set_name(std::string& name);
 
 int main(){
     std::string name = "Bob";
@@ -16,4 +19,16 @@ int main(){
     std::cout << "pName = " << pName << std::endl;
     std::cout << "*pName = " << *pName << std::endl;
     return 0;
+}
+
+std::string setName(){
+    std::string name;
+    std::cout << "Enter name: ";
+    std::cin >> name;
+    return name;
+}
+
+void set_name(std::string& name) {
+    std::cout << "Enter name: ";
+    std::cin >> name;
 }
